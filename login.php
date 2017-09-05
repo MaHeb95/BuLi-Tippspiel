f82037e3eba798c3d656c200d66d1a61c2f70843
Mark Hebestreit <hebestreit.mark@gmail.com>
2017-09-05 13:11:13 +0200

    fix bug

--- login.php
+++ login.php
@@ -19,8 +19,8 @@ if(isset($_GET['login'])) {
     //Überprüfung des Passworts
     if ($user !== false && password_verify($password, $user['password'])) {
         $_SESSION['userid'] = $user['id'];
-        //die('Login erfolgreich. Weiter zu <a href="start.php">internen Bereich</a>');
-        header();
+        die('Login erfolgreich. Weiter zu <a href="start.php">internen Bereich</a>');
+        //header();
     } else {
         $errorMessage = "Username oder Passwort war ungültig<br>";
     }
