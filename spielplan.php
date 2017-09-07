f5cbbf41afedfa62d9b2b1fb6aeb9f43be1161bf
Mark Hebestreit <hebestreit.mark@gmail.com>
2017-09-07 19:31:30 +0200

    Update spielplan.php

--- spielplan.php
+++ spielplan.php
@@ -50,48 +50,59 @@ if (isset($_GET["season"]) && is_numeric($_GET["season"])) {
 <?php
 $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
+
 <form class="form" id="form" name="form" method="post" action="<?php echo $actual_link; ?>">
     <fieldset>
-        <p class="bg">
-            <label for="season">Wähle eine Saison</label> <!-- Season SELECTION -->
-            <!--onChange event fired and function autoSubmit() is invoked-->
-            <select id="season" name="season" onchange="autoSubmit();">
-                <option value="">-- Wähle eine Saison --</option>
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-3">
+                    <p class="bg">
+                        <label for="season">Wähle eine Saison</label> <!-- Season SELECTION -->
+                            <!--onChange event fired and function autoSubmit() is invoked-->
+                            <select class="form-control" id="season" name="season" onchange="autoSubmit();">
+                            <option value="">-- Wähle eine Saison --</option>
+                            <?php
+                            //select Season. Seasons are with parent_id=0
+                            $sql = "select id,name from soccer_pool.season ";
+                            $result = dbQuery($sql);
+                            while ($row = dbFetchAssoc($result)) {
+                                echo ("<option value=\"{$row['id']}\" " . ($seasonmenu == $row['id'] ? " selected" : "") . ">{$row['name']}</option>");
+                            }
+                            ?>
+                        </select>
+                    </p>
+                </div>
                 <?php
-                //select Season. Seasons are with parent_id=0
-                $sql = "select id,name from soccer_pool.season ";
-                $result = dbQuery($sql);
-                while ($row = dbFetchAssoc($result)) {
-                    echo ("<option value=\"{$row['id']}\" " . ($seasonmenu == $row['id'] ? " selected" : "") . ">{$row['name']}</option>");
-                }
-                ?>
-            </select>
-        </p>
-        <?php
-        //check whether Season was really selected and Season id is numeric
-        if ($seasonmenu != '' && is_numeric($seasonmenu)) {
-            ////select sub-categories categories for a given Season id
-            $sql = "select id,name from soccer_pool.matchday where season_id=" . $seasonmenu;
-            $result = dbQuery($sql);
-            if (dbNumRows($result) > 0) {
-                ?>
-                <p class="bg">
-                    <label for="matchday">Wähle einen Spieltag</label>
-                    <select id="matchday" name="matchday">
-                        <option value="">-- Wähle einen Spieltag --</option>
-                        <?php
-                        //POPULATE DROP DOWN WITH Matchday FROM A GIVEN Season
-                        while ($row = dbFetchAssoc($result)) {
-                            echo ("<option value=\"{$row['id']}\" " . ($matchdaymenu == $row['id'] ? " selected" : "") . ">{$row['name']}</option>");
-                        }
+                //check whether Season was really selected and Season id is numeric
+                if ($seasonmenu != '' && is_numeric($seasonmenu)) {
+                    ////select sub-categories categories for a given Season id
+                    $sql = "select id,name from soccer_pool.matchday where season_id=" . $seasonmenu;
+                    $result = dbQuery($sql);
+                    if (dbNumRows($result) > 0) {
                         ?>
-                    </select>
-                </p>
+                <div class="col col-lg-3">
+                    <p class="bg">
+                        <label for="matchday">Wähle einen Spieltag</label>
+                        <select class="form-control" id="matchday" name="matchday">
+                            <option value="">-- Wähle einen Spieltag --</option>
+                            <?php
+                            //POPULATE DROP DOWN WITH Matchday FROM A GIVEN Season
+                            while ($row = dbFetchAssoc($result)) {
+                                echo ("<option value=\"{$row['id']}\" " . ($matchdaymenu == $row['id'] ? " selected" : "") . ">{$row['name']}</option>");
+                            }
+                            ?>
+                        </select>
+                    </p>
+                </div>
                 <?php
             }
         }
         ?>
-        <p><input name="submit" value="Submit" type="submit" /></p>
+                <div class="col col-lg-2">
+                    <p><input class="btn btn-info" class="col align-self-end" name="submit" value="Submit" type="submit" /></p>
+                </div>
+            </div>
+        </div>
     </fieldset>
 </form>
 
@@ -110,9 +121,52 @@ if (isset($_POST['submit'])) {
         echo 'Season Id: ' . $seasonmenu;
     }
 }
-
-var_dump(get_matches(get_match_ids($matchdaymenu)));
+$md_matches = get_matches(get_match_ids($matchdaymenu));
+var_dump($md_matches);
 ?>
+
+<table class="table">
+    <thead class="thead-inverse">
+    <tr>
+        <th>#</th>
+        <th>Anstoss</th>
+        <th>Heim</th>
+        <th>Gast</th>
+        <th>Heimtore</th>
+        <th>Gasttore</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+<?php
+if($md_matches->rowCount() > 0){
+    foreach($md_matches AS $row) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['start_tim'] . "</td>";
+        echo "<td>" . $row['home_team'] . "</td>";
+        echo "<td>" . $row['guest_team'] . "</td>";
+        echo "<td>" . $row['home_goals'] . "</td>";
+        echo "<td>" . $row['guest_goals'] . "</td>";
+        echo "<td></td>";
+        echo "</tr>";
+        echo "</tbody>";
+        echo "</table>";
+    }}
+else {
+    echo "<p class='lead'><em>Keine Spiele gefunden.</em></p>";
+}?>
+
+<div class="container">
+    <form action="" method="post">
+        <label for="inputurl">Eingabe der Match URL</label>
+        <input type="text" class="form-control" id="inputurl" placeholder="URL" value="<?php echo $url; ?>">
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+<?php
+var_dump(trim($_POST['inputurl']));
+//parse_match_url($url); ?>
 
 
 </body>
